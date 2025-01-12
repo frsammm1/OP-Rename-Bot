2fef54c4cd64d50a9de08453d904f55c92bb15e3
frsammm1 <thefatherofficial@gmail.com>
2025-01-12 16:54:08 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,12 +12,12 @@ logging.basicConfig(
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = int(os.environ.get("API_ID", "20984532"))
+    API_HASH = os.environ.get("API_HASH", "ab43b53b8201801da5efb910fd7c62c9")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 5641314567)
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
     MONGODB_URI = os.environ.get("MONGODB_URI", "")
